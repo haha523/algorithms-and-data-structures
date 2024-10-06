1d09096c0f329cb17c38846bca52fa80e60b547f
Хыу Жанг haha234@ <77500794+haha523@users.noreply.github.com>
2024-10-06 18:58:25 +0300

    Update vigenre.py

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -9,7 +9,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()  
+    keyword_length = len(keyword)
+    
+    for i, char in enumerate(plaintext):
+        if char.isalpha():  
+            shift = ord(keyword[i % keyword_length]) - ord('A')
+            if char.isupper():
+                ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            elif char.islower():
+                ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += char 
+    
     return ciphertext
 
 
@@ -24,5 +36,25 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()  
+    keyword_length = len(keyword) 
+    
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():  
+            shift = ord(keyword[i % keyword_length]) - ord('A')
+            if char.isupper():
+                plaintext += chr((ord(char) - ord('A') - shift + 26) % 26 + ord('A'))
+            elif char.islower():
+                plaintext += chr((ord(char) - ord('a') - shift + 26) % 26 + ord('a'))
+        else:
+            plaintext += char  
+    
     return plaintext
+
+plaintext = input("Простой текст: ")
+keyword = input("Ключ: ")
+ciphertext = encrypt_vigenere(plaintext, keyword)
+print("Зашифрованный текст: " , ciphertext)  
+
+decrypted_text = decrypt_vigenere(ciphertext, keyword)
+print("Декодируется текст: " , decrypted_text)  
