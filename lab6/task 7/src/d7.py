6b495691544f403b3a1a16ba928348961767f2b7
Хыу Жанг haha234@ <77500794+haha523@users.noreply.github.com>
2024-12-28 18:10:49 +0300

    Update

--- lab6/task 7/src/d7.py
+++ lab6/task 7/src/d7.py
@@ -1,1 +1,37 @@
+import os
+from collections import defaultdict
 
+def read_input(file_path):
+    with open(file_path, 'r') as f:
+        n, k = map(int, f.readline().strip().split())
+        stones = f.readline().strip()
+        pairs = [tuple(f.readline().strip()) for _ in range(k)]
+    return n, k, stones, pairs
+
+def write_output(file_path, result):
+    with open(file_path, 'w') as f:
+        f.write(str(result) + "\n")
+
+def count_beautiful_pairs(n, stones, pairs):
+    count = 0
+
+    stone_count = defaultdict(int)
+
+    for i in range(n):
+        for a, b in pairs:
+            if stones[i] == b:
+                count += stone_count[a]
+        stone_count[stones[i]] += 1
+
+    return count
+
+def main():
+    input_path = os.path.join('..', 'txtf', 'input.txt')
+    output_path = os.path.join('..', 'txtf', 'output.txt')
+
+    n, k, stones, pairs = read_input(input_path)
+    result = count_beautiful_pairs(n, stones, pairs)
+    write_output(output_path, result)
+
+if __name__ == "__main__":
+    main()
